719b7f294fccc00b24a1d3d7dd4556556359c6d9
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2017-10-11 21:06:14 +0800

    make register-server as server.

---
+++ register-server/src/main/java/org/register/server/RegisterServer.java
@@ -0,0 +1,24 @@
+package org.register.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * refer: 
+ * <li> http://www.baeldung.com/spring-cloud-netflix-eureka
+ * <li> https://www.mkyong.com/spring-boot/spring-boot-profile-based-properties-and-yaml-example/
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+//@EnableHystrix
+@EnableHystrixDashboard
+public class RegisterServer 
+{
+    public static void main( String[] args )
+    {
+       SpringApplication.run(RegisterServer.class, args);
+    }
+}
