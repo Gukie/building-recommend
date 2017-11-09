f97321d70b8f32a4e1b1f4ce141f7b6f1d6c8dbe
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2017-11-09 18:05:33 +0800

    ELK Junit test successfully.

--- data-service/src/main/java/org/data/service/impl/ElkDataServiceImpl.java
+++ data-service/src/main/java/org/data/service/impl/ElkDataServiceImpl.java
@@ -1,10 +1,14 @@
 package org.data.service.impl;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import com.alibaba.fastjson.JSON;
 
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
+import org.common.constant.SpecialValues;
 import org.common.enums.IndexTypeEnum;
 import org.common.model.BuildingDTO;
 import org.data.service.ElkDataService;
@@ -24,9 +28,23 @@ public class ElkDataServiceImpl implements ElkDataService {
 	private String host;
 	private Integer port;
 	private String scheme; //http, https
+	private String username ;
+	private String password ;
 	private RestHighLevelClient restClient;
 	private RestClient lowLevelClient;
 
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public String getHost() {
 		return host;
 	}
@@ -68,12 +86,27 @@ public class ElkDataServiceImpl implements ElkDataService {
 		IndexRequest request = new IndexRequest(IndexTypeEnum.building.getValue(),"test");
 		request.source(JSON.toJSONString(buildingDTO), XContentType.JSON);
 		try {
-			return restClient.index(request);
+			Header header = new BasicHeader("Authorization",generateToken());
+			return restClient.index(request,header);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		return null;
+	}
+	/**
+	 * refer: 
+	 * <li> https://www.elastic.co/guide/en/shield/current/_using_elasticsearch_http_rest_clients_with_shield.html
+	 * <li> https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication
+	 * @return
+	 */
+	private String generateToken() {
+		StringBuilder encodStr = new StringBuilder();
+		encodStr.append(username);
+		encodStr.append(SpecialValues.COLON_STR);
+		encodStr.append(password);
+		String encodedToken = Base64.getEncoder().encodeToString(encodStr.toString().getBytes());
+		return "Basic "+encodedToken;
 	}
 	
 }
