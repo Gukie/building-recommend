38552779eb354cc0249cd3f31faae30b23f1c0cd
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2017-11-13 18:11:38 +0800

    add junit test, but not pass

--- data-service/src/test/java/org/data/service/MongoDataServiceTest.java
+++ data-service/src/test/java/org/data/service/MongoDataServiceTest.java
@@ -1,10 +1,24 @@
 package org.data.service;
 
+import org.common.model.BuildingDTO;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author gushu
  * @date 2017/11/13
  */
 public class MongoDataServiceTest extends ServiceBaseTest{
 
-//	private Mongo
+	@Autowired
+	private MongoDataService dataService;
+	
+	@Test
+	public void testAdd(){
+		BuildingDTO buildingDTO = new BuildingDTO();
+		buildingDTO.setName("junit test");
+		buildingDTO.setLocation("yuhang");
+		buildingDTO.setPlate("no");
+		dataService.addDocument(buildingDTO);
+	}
 }
